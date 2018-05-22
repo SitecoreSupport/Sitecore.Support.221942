1be59c2f6528acb84cc378a4beebd27ef12dc35b
DK\AMCH <amch@sitecore.net>
2018-05-22 09:46:39 +0300

    commit initial code

---
+++ src/Sitecore.Support.221942/FXM/BeaconLoaderProcessor.cs
@@ -0,0 +1,44 @@
+﻿using Sitecore.Abstractions;
+using Sitecore.Configuration;
+using Sitecore.FXM.Abstractions;
+using Sitecore.FXM.Pipelines.Bundle;
+using Sitecore.FXM.Utilities;
+using Sitecore.StringExtensions;
+using System;
+using System.Web.Optimization;
+
+namespace Sitecore.Support.FXM
+{
+    public class BeaconLoaderProcessor : IBundleProcessor<OnRequestBundleGeneratorArgs>
+    {
+        private readonly IFileUtil fileUtil;
+        private readonly string protocol;
+        private readonly string service;
+        private readonly ISettings settings;
+
+        public BeaconLoaderProcessor() : this(new SettingsWrapper(), new FileUtilWrapper())
+        {
+        }
+
+        public BeaconLoaderProcessor(ISettings settings, IFileUtil fileUtil)
+        {
+            this.settings = settings;
+            this.fileUtil = fileUtil;
+            this.protocol = this.settings.GetSetting("FXM.Protocol", string.Empty);
+            this.service = "Beacon.Service".Replace('.', '/');
+        }
+
+        public void Process(OnRequestBundleGeneratorArgs args)
+        {
+            string setting = this.settings.GetSetting("Sitecore.Services.RouteBase", "sitecore/api/ssc/");
+
+            string uriHost = FxmUtility.GetUriHost(args.Context.Request.Url);
+
+            string str3 = "{0}{1}/{2}/{3}".FormatWith(new object[] { this.protocol, uriHost.TrimEnd(new char[] { '/' }), setting.TrimEnd(new char[] { '/' }), this.service });
+            string uniqueFilename = this.fileUtil.GetUniqueFilename(Settings.TempFolderPath + "/beacon.js");
+            string text = "SCBeacon = new SCBeacon(\"" + str3 + "\");";
+            this.fileUtil.WriteToFile(uniqueFilename, text, true);
+            args.Bundle.Include("~" + uniqueFilename, new IItemTransform[0]);
+        }
+    }
+}
